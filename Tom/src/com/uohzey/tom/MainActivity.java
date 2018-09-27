5a565f25a4b90ee9c4a85bfa7b6a194151e6d808
uohzey <420709076@qq.com>
2018-09-27 16:55:46 +0800

    complete

--- Tom/src/com/uohzey/tom/MainActivity.java
+++ Tom/src/com/uohzey/tom/MainActivity.java
@@ -1,6 +1,11 @@
 package com.uohzey.tom;
 
+import java.util.HashMap;
+
 import android.R.integer;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
@@ -17,9 +22,10 @@ public class MainActivity extends Activity {
 	private RelativeLayout bg;
 	private Button lookBtn;
 	private Button talkBtn;
-	private Button fartBtn;
-	
+	private Button fartBtn;	
+	private SoundPool soundPool=null;
 	private Handler handler=null;
+	private HashMap<Integer, Integer> hashMap=null;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -27,85 +33,37 @@ public class MainActivity extends Activity {
 		initView();
 		initBreathAnim();
 		handler=new Handler();
+		hashMap=new HashMap<Integer, Integer>();
+		soundPool=new SoundPool(4, AudioManager.STREAM_MUSIC,0);
+		hashMap.put(1, soundPool.load(MainActivity.this, R.raw.p_knockout3,1));
+		hashMap.put(2, soundPool.load(MainActivity.this, R.raw.p_poke_foot3,1));
+		hashMap.put(3, soundPool.load(MainActivity.this, R.raw.p_poke_tail13,1));
+		hashMap.put(4, soundPool.load(MainActivity.this, R.raw.pillow5,1));
 		lookBtn.setOnClickListener(new OnClickListener() {
-			
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				bg.setBackgroundResource(R.drawable.look_animal);
-				AnimationDrawable AnimationDrawable=(AnimationDrawable) bg.getBackground();
-				//开始播放动画
-				AnimationDrawable.setOneShot(true);
-				AnimationDrawable.start();
-				int time=0;
-				//AnimationDrawable.getNumberOfFrames() 得到动画中所有帧数
-				for (int i = 0; i < AnimationDrawable.getNumberOfFrames(); i++) {
-					time+=AnimationDrawable.getDuration(i);
-				}
-				handler.postDelayed(new Runnable() {
-					
-					@Override
-					public void run() {
-						bg.setBackgroundResource(R.drawable.breath_animal);
-						AnimationDrawable bgAnim= (AnimationDrawable) bg.getBackground();
-						bgAnim.start();
-						
-					}
-				}, time);
-			}
+				/**
+				 * soundID播放源
+				 * leftValume左声道
+				 * rightValume右声道
+				 */
+				soundPool.play(hashMap.get(1), 1, 1, 0, 0,1);
+				startAnim(R.drawable.look_animal);
+			}			
 		});
 		talkBtn.setOnClickListener(new OnClickListener() {
-			
 			@Override
+			
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				bg.setBackgroundResource(R.drawable.talk_animal);
-				AnimationDrawable AnimationDrawable=(AnimationDrawable) bg.getBackground();
-				//开始播放动画
-				AnimationDrawable.setOneShot(true);
-				AnimationDrawable.start();
-				int time=0;
-				//AnimationDrawable.getNumberOfFrames() 得到动画中所有帧数
-				for (int i = 0; i < AnimationDrawable.getNumberOfFrames(); i++) {
-					time+=AnimationDrawable.getDuration(i);
-				}
-				handler.postDelayed(new Runnable() {
-					
-					@Override
-					public void run() {
-						bg.setBackgroundResource(R.drawable.breath_animal);
-						AnimationDrawable bgAnim= (AnimationDrawable) bg.getBackground();
-						bgAnim.start();
-						
-					}
-				}, time);
+				soundPool.play(hashMap.get(2), 1, 1, 0, 0,1);
+				startAnim(R.drawable.talk_animal);
 			}
 		});
 		fartBtn.setOnClickListener(new OnClickListener() {
-			
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				bg.setBackgroundResource(R.drawable.fart_animal);
-				AnimationDrawable AnimationDrawable=(AnimationDrawable) bg.getBackground();
-				//开始播放动画
-				AnimationDrawable.setOneShot(true);
-				AnimationDrawable.start();
-				int time=0;
-				//AnimationDrawable.getNumberOfFrames() 得到动画中所有帧数
-				for (int i = 0; i < AnimationDrawable.getNumberOfFrames(); i++) {
-					time+=AnimationDrawable.getDuration(i);
-				}
-				handler.postDelayed(new Runnable() {
-					
-					@Override
-					public void run() {
-						bg.setBackgroundResource(R.drawable.breath_animal);
-						AnimationDrawable bgAnim= (AnimationDrawable) bg.getBackground();
-						bgAnim.start();
-						
-					}
-				}, time);
+				soundPool.play(hashMap.get(3), 1, 1, 0, 0,1);
+				startAnim(R.drawable.fart_animal);
 			}
 		});
 	}
@@ -125,12 +83,39 @@ public class MainActivity extends Activity {
 		fartBtn=(Button) findViewById(R.id.fart_btn);
 		
 	}
-
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
+	private void startAnim(int resId) {
+		lookBtn.setClickable(false);
+		talkBtn.setClickable(false);
+		fartBtn.setClickable(false);
+		// TODO Auto-generated method stub
+		bg.setBackgroundResource(resId);
+		AnimationDrawable AnimationDrawable=(AnimationDrawable) bg.getBackground();
+		//开始播放动画
+		AnimationDrawable.setOneShot(true);
+		AnimationDrawable.start();
+		int time=0;
+		//AnimationDrawable.getNumberOfFrames() 得到动画中所有帧数
+		for (int i = 0; i < AnimationDrawable.getNumberOfFrames(); i++) {
+			time+=AnimationDrawable.getDuration(i);
+		}
+		handler.postDelayed(new Runnable() {
+			
+			@Override
+			public void run() {
+				lookBtn.setClickable(true);
+				talkBtn.setClickable(true);
+				fartBtn.setClickable(true);
+				bg.setBackgroundResource(R.drawable.breath_animal);
+				AnimationDrawable bgAnim= (AnimationDrawable) bg.getBackground();
+				bgAnim.start();
+				
+			}
+		}, time);
+	}
 }
